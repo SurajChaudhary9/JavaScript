1df601a0541321b555241aace1413717a537af28
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-06-28 00:55:47 +0530

    Added : Maps Function

--- maps/map.js
+++ maps/map.js
@@ -1,12 +1,12 @@
-function map(arr,mapFn){
-  const mapArr=[];
-  for(let i=0;i<arr.length;i++){
-    const result=mapFn(arr[i]);
+function map(arr, mapFn) {
+  const mapArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    const result = mapFn(arr[i]); //0,1,2,3
     mapArr.push(result);
   }
   return mapArr;
 }
 
-let arr=[1,2,3,4];
-arr=map(arr,(num)=>num**2);
+let arr = [2, 3, 4, 5];
+arr = map(arr, (num) => num ** 2); //^2=**2
 console.log(arr);
